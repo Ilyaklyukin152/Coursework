4ba2d2fff1e77db2a27de0df5a2bcc9f2610a9ae
Илья <git@github.com:Ilyaklyukin152/helloworld.git>
2025-02-17 21:07:10 +0300

    Coursework done

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,8 @@ public class Main {
         System.out.println();
         printAllEmployees(employees);
         System.out.println();
+        indexSalaryDepartment(employees,3,15);
+        System.out.println();
         System.out.println(findEmployeeMinSalaryDepartment(employees, 3));
         System.out.println();
         System.out.println(findEmployeeMaxSalaryDepartment(employees, 5));
